d4db5d2dd5087aae36fda8d473f5e7f9f0585a1a
Snoweuph <snow+github@euph.email>
2022-12-26 08:31:38 +0100

    Addin polybar modules

---
+++ polybar/scripts/dunst_snooze.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+case "$1" in
+    --toggle)
+	dunstctl set-paused toggle
+        ;;
+    *)
+      	if [ "$(dunstctl is-paused)" = "true" ]; then
+            echo "%{F$2} $(dunstctl count waiting)"
+        else
+            echo "%{F$1} $(dunstctl count displayed)"
+        fi
+	;;
+esac
---
+++ polybar/scripts/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+killall -q polybar
+
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
