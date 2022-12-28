f4503e68df5a088dc98f28938cad56bfb5630fb2
Snoweuph <snow+github@euph.email>
2022-12-28 17:55:01 +0100

    fixes

--- rice-bowl/scripts/generators/generate_i3_config.sh
+++ rice-bowl/scripts/generators/generate_i3_config.sh
@@ -3,37 +3,6 @@
 RICE_I3_PATH=$RICE_DIR_CONFIG/i3
 RICE_I3_DIR_CONF=$RICE_I3_PATH/conf
 
-# Generate colors.conf
-echo "# $RICE_GENERATED_HEADER
-set \$color_d4 \"$RICE_COLOR_DARK_4\"
-set \$color_d3 \"$RICE_COLOR_DARK_3\"
-set \$color_d2 \"$RICE_COLOR_DARK_2\"
-set \$color_d1 \"$RICE_COLOR_DARK_1\"
-
-set \$color_l1 \"$RICE_COLOR_LIGHT_1\"
-set \$color_l2 \"$RICE_COLOR_LIGHT_2\"
-set \$color_l3 \"$RICE_COLOR_LIGHT_3\"
-set \$color_l4 \"$RICE_COLOR_LIGHT_4\"
-
-set \$color_red \"$RICE_COLOR_RED\"
-set \$color_orange \"$RICE_COLOR_ORANGE\"
-set \$color_yellow \"$RICE_COLOR_YELLOW\"
-set \$color_lime \"$RICE_COLOR_LIME\"
-set \$color_green \"$RICE_COLOR_GREEN\"
-set \$color_cyan \"$RICE_COLOR_CYAN\"
-set \$color_light_blue \"$RICE_COLOR_LIGHT_BLUE\"
-set \$color_blue \"$RICE_COLOR_BLUE\"
-set \$color_purple \"$RICE_COLOR_PURPLE\"
-set \$color_magenta \"$RICE_COLOR_MAGENTA\"
-
-set \$color_accent_primary \"$RICE_COLOR_PRIMARY_ACCENT\"
-set \$color_accent_secondary \"$RICE_COLOR_SECONDARY_ACCENT\"
-set \$color_accent_tertiary \"$RICE_COLOR_TERTIARY_ACCENT\"
-
-#Include next variable file
-include ./styling.nogit.conf
-" > $RICE_I3_DIR_CONF/colors.nogit.conf
-
 # Generate fonts.conf
 echo "# $RICE_GENERATED_HEADER
 set \$font_normal $RICE_FONT_NORMAL
@@ -51,18 +20,24 @@ set \$font_size_icons_big $RICE_FONT_SIZE_ICONS_BIG
 font pango:\$font_mono \$font_size_small
 " > $RICE_I3_DIR_CONF/fonts.nogit.conf
 
-# Generate styling2.conf
+# Generate styling.conf
 echo "# $RICE_GENERATED_HEADER
-set \$padding $RICE_PADDING
-set \$gap_size $RICE_GAPS
 
-set \$border_radius $RICE_BORDER_RADIUS
-set \$border_size $RICE_BORDER_THICKNESS
+# Configure Gaps
+gaps inner $RICE_GAPS
+# Configure Border Size
+default_border pixel $RICE_BORDER_THICKNESS
+default_floating_border pixel $RICE_BORDER_THICKNESS
 
-set \$opacity $RICE_OPACITY_FLOAT
+# Confgire Colors
+# class                 border          		        backgr.         text            indicator               	child_border
+client.focused          $RICE_COLOR_PRIMARY_ACCENT		$RICE_COLOR_DARK_2		$RICE_COLOR_LIGHT_1		$RICE_COLOR_PRIMARY_ACCENT		$RICE_COLOR_LIGHT_1
+client.focused_inactive $RICE_COLOR_SECONDARY_ACCENT	$RICE_COLOR_DARK_2		$RICE_COLOR_LIGHT_1		$RICE_COLOR_SECONDARY_ACCENT	$RICE_COLOR_LIGHT_1
+client.unfocused        $RICE_COLOR_DARK_1			    $RICE_COLOR_DARK_3		$RICE_COLOR_LIGHT_1		$RICE_COLOR_DARK_1				$RICE_COLOR_LIGHT_1
+client.urgent           $RICE_COLOR_RED			       	$RICE_COLOR_ORANGE		$RICE_COLOR_RED			$RICE_COLOR_ORANGE				$RICE_COLOR_LIGHT_1
+client.placeholder	    $RICE_COLOR_LIGHT_1			   	$RICE_COLOR_LIGHT_3		$RICE_COLOR_DARK_1		$RICE_COLOR_LIGHT_1				$RICE_COLOR_LIGHT_1
 
-#Include Stylings
-include ./stylings.conf
+client.background	    $RICE_COLOR_LIGHT_1
 " > $RICE_I3_DIR_CONF/styling.nogit.conf
 
 # Generate apps2.conf
