845463466644293e4984a3af578838cdd298c0c4
Snoweuph <snow+github@euph.email>
2022-12-28 17:14:09 +0100

    fixing scripts

--- rice-bowl/scripts/generators/generate_i3_config.sh
+++ rice-bowl/scripts/generators/generate_i3_config.sh
@@ -84,7 +84,7 @@ bindsym \$mod+Shift+s exec --no-startup-id flameshot gui --clipboard --accept-on
 exec --no-startup-id picom -b --config $RICE_DIR_CONFIG/picom/picom.nogit.conf --experimental-backend
 
 # start polybar
-exec_always $RICE_DIR_CONFIG/polybar/launch.sh
+exec_always $RICE_DIR_CONFIG/polybar/scripts/launch.sh
 
 # set wallpaper
 exec --no-startup-id $RICE_DIR_CONFIG/.fehbg
