7085cdc29126b73886e07181cfe7c0299793e3b8
Snoweuph <snow+github@euph.email>
2023-01-10 18:27:53 +0100

    improve installing

--- rice-bowl/scripts/install.sh
+++ rice-bowl/scripts/install.sh
@@ -4,6 +4,11 @@ SH_DIR_INSTALL=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pw
 
 source $SH_DIR_INSTALL/../config.sh
 
+# If Config Folder Exists rename it first
+sudo mv $RICE_DIR_CONFIG ${RICE_DIR_CONFIG}_old
+# Create Config Folder
+mkdir $RICE_DIR_CONFIG
+
 # Move Whole Repo Folder to Rice Location
 sudo mv $SH_DIR_INSTALL/../../* -t=$RICE_DIR_CONFIG
 # Move All Hiden Files
